b5c84a6327e141aa5f28135e9f5dd530541124ff
Ronald Domi <ronalddomi4@gmail.com>
2020-12-13 13:13:25 +0100

    cell placement with direction

--- screen.py
+++ screen.py
@@ -1,8 +1,8 @@
 
 import pygame
 import os
-from game import bonus_board
 from screen_helpers import *
+from input import TextInput
 pygame.init()
 pygame.display.set_caption("Scrabble")
 os.environ['SDL_VIDEO_CENTERED'] = '1'
@@ -11,52 +11,169 @@ height = 700
 screen = pygame.display.set_mode((width, height))
 screen.fill((50, 50, 50))
 
+
+board_list = init_jeton()
+bonus_board = init_bonus_board()
+# drawing parts
 draw_starting_board(screen, bonus_board)
-draw_console(screen)
-draw_text(screen, 760, 260, 'CONSOLE', [255, 255, 255], 'Corbel', 26)
-draw_text(screen, 760, 360, 'CONSOLE', [255, 255, 255], 'Corbel', 26)
-draw_text(screen, 760, 460, 'CONSOLE', [255, 255, 255], 'Corbel', 26)
-# draw_text(x, y, text, color, text_font, size)
-# 750, 250, 400, 450
 
 button_width = 130
 button_height = 40
 button_font = "Corbel"
-# ----------- BUTTON 1
-button1_x = 780
-button1_y = 10
-text_color = [0, 0, 0]
-show_button(screen, button1_x, button1_y, button_width,
-            button_height, [100, 100, 100], button_font, 'hello guys', 18, text_color)
-
-# ----------- BUTTON 2
-button2_x = 780
-button2_y = 70
-text_color = [0, 0, 0]
-show_button(screen, button2_x, button2_y, button_width,
-            button_height, [100, 100, 100], button_font, 'Skip', 18, text_color)
-
-# ----------- BUTTON 3
-button3_x = 780
-button3_y = 130
-text_color = [0, 0, 0]
-show_button(screen, button3_x, button3_y, button_width,
-            button_height, [100, 100, 100], button_font, 'Exchange', 18, text_color)
-
-# ----------- BUTTON 3
-button4_x = 780
-button4_y = 190
-text_color = [0, 0, 0]
-show_button(screen, button4_x, button4_y, button_width,
-            button_height, [100, 100, 100], button_font, 'Place', 18, text_color)
+buttons_cords = draw_buttons(screen, button_width, button_height, button_font)
+button2_x, button2_y = buttons_cords[0][0], buttons_cords[0][1]
+button3_x, button3_y = buttons_cords[1][0], buttons_cords[1][1]
+button4_x, button4_y = buttons_cords[2][0], buttons_cords[2][1]
+button5_x, button5_y = buttons_cords[3][0], buttons_cords[3][1]
+# drawing parts
 
 pygame.display.flip()
 
+# player variable
+letters_dictionary = letters_dico()
+list_of_all_possible_words = generer_dico('documents/littre.txt')
+sac = init_sac(letters_dictionary)
 
+players_dico = {}
+# player1 = input("first player ")
+player1 = "Farah"
+# player2 = input("second player ")
+player2 = "Ronald"
+hand1 = piocher(10, sac)
+hand2 = piocher(10, sac)
+score1 = 0
+score2 = 0
+
+players_dico[player1] = {}
+players_dico[player1]['hand'] = hand1
+players_dico[player1]['score'] = score1
+players_dico[player2] = {}
+players_dico[player2]['hand'] = hand2
+players_dico[player2]['score'] = score2
+
+turn = 1
+textinput = TextInput(text_color=(255, 255, 255), cursor_color=(255, 255, 255))
+
+
+def generate_console():
+    """
+        returns the current_turn playerName, and available words of the current_player\n
+    """
+    playerName = ''
+    available_words = []
+    if turn % 2 == 0:
+        playerName = player1
+        available_words = mots_jouables(
+            list_of_all_possible_words, players_dico[playerName]['hand'], 0)
+        if available_words == []:
+            print("You cannot make a word with the avalable letters")
+            # do something
+        best_words = meilleurs_mots(
+            available_words, players_dico[playerName]['hand'], letters_dictionary)
+        player_console(
+            screen, playerName, players_dico[playerName]['hand'], players_dico[playerName]['score'], best_words)
+
+    else:
+        playerName = player2
+        available_words = mots_jouables(
+            list_of_all_possible_words, players_dico[playerName]['hand'], 0)
+        if available_words == []:
+            print("You cannot make a word with the avalable letters")
+            # do something
+        best_words = meilleurs_mots(
+            available_words, players_dico[playerName]['hand'], letters_dictionary)
+        player_console(
+            screen, playerName, players_dico[playerName]['hand'], players_dico[playerName]['score'], best_words)
+    return playerName, available_words
+
+
+generate_console()
 Playing = True
+userInput = ''
+playerName = ''
+is_console_generated = False
+waiting_cell = "None"
 while Playing:
-    Playing = handle_exit()
+    events = pygame.event.get()
+    Playing = handle_exit(events)
+    if is_console_generated == False:
+        print('console generated')
+        draw_console(screen)
+        current_player_name, available_words = generate_console()
+        is_console_generated = True
+
     if not Playing:
         break
-    if is_button_pressed(button1_x, button1_y, button_width, button_height):
-        print("don't touch me, ")
+
+    if waiting_cell[0:4] == "True":
+        direction = waiting_cell[5:]
+        print(direction)
+        cellX_index, cellY_index = get_cell_pressed(events)
+        if cellX_index != -1 and cellY_index != -1:
+            waiting_cell = "False"
+        if waiting_cell == "False":
+            if direction == 'Horizontal':
+                is_placed = placer_mot(
+                    board_list, players_dico[playerName]['hand'], userInput, cellY_index+1, cellX_index +
+                    1, 'horizontal')
+                update_board(screen, cellY_index, cellX_index,
+                             'horizontal', userInput)
+            else:
+                is_placed = placer_mot(
+                    board_list, players_dico[playerName]['hand'], userInput, cellY_index+1, cellX_index +
+                    1, 'vertical')
+                update_board(screen, cellY_index, cellX_index,
+                             'vertical', userInput)
+            print_list(board_list)
+
+            textinput.input_string = ''
+            is_console_generated = False
+            turn += 1
+
+    pygame.display.flip()
+    # skip
+    if is_button_pressed(events, button2_x, button2_y, button_width, button_height):
+        turn += 1
+        is_console_generated = False
+        textinput.input_string = ''
+    # exchange
+    elif is_button_pressed(events, button3_x, button3_y, button_width, button_height):
+
+        is_console_generated = False
+
+        textinput.input_string = ''
+        playerName, _ = generate_console()
+        if userInput != '':
+            selected_jetons = list(userInput)
+            updated_hand = echanger(
+                selected_jetons, players_dico[playerName]['hand'], sac)[0]
+            players_dico[playerName]['hand'] = updated_hand
+        turn += 1
+    # placement horizontal
+    elif is_button_pressed(events, button4_x, button4_y, button_width, button_height):
+        # is_console_generated = False
+        playerName, available_words = generate_console()
+        if userInput not in available_words:
+            print("Word does not exist, try to enter again")
+            turn -= 1
+        else:
+            print("your word is correct")
+
+        waiting_cell = "True Horizontal"
+
+    # placement vertical
+    elif is_button_pressed(events, button5_x, button5_y, button_width, button_height):
+        # is_console_generated = False
+        playerName, available_words = generate_console()
+        if userInput not in available_words:
+            print("Word does not exist, try to enter again")
+            turn -= 1
+        else:
+            print("your word is correct")
+
+        waiting_cell = "True Vertical"
+    userInput = textinput.get_text()
+    textinput.update(events)
+
+    draw_buttons(screen, button_width, button_height, button_font)
+    screen.blit(textinput.get_surface(), (920, 130))
--- screen_helpers.py
+++ screen_helpers.py
@@ -1,5 +1,8 @@
 
 import pygame
+from functions import *
+
+import types
 
 cell_width = 50
 cell_color_fill = cell_width - 5
@@ -46,7 +49,40 @@ def draw_starting_board(screen, bonus_board):
             screen.blit(img, (x, y))
 
 
+def update_board(screen, row, column, direction, mot):
+    """
+        draws the cells, with their respective colors\n
+        adds the name of the bonus above, if it has one \n
+        and blits the cells
+    """
+    i = 0
+    length = len(mot)
+    list_of_cells_to_be_updated = []
+    for times in range(length):
+        if direction == 'horizontal':
+            list_of_cells_to_be_updated.append([row, column + times])
+        if direction == 'vertical':
+            list_of_cells_to_be_updated.append([row + times, column])
+
+    for rowIndex in range(14):
+        for cellIndex in range(14):
+            if [rowIndex, cellIndex] in list_of_cells_to_be_updated:
+                cell = pygame.Rect(cell_width * cellIndex,
+                                   cell_width * rowIndex, cell_color_fill, cell_color_fill)
+
+                pygame.draw.rect(screen, [255, 255, 255], cell)
+                font = pygame.font.SysFont(None, 35)
+                img = font.render(mot[i], True, [0, 0, 0])
+                x = int(cell_width * cellIndex + cell_width / 5)
+                y = int(cell_width * rowIndex + cell_width / 5)
+                screen.blit(img, (x, y))
+                i += 1
+
+
 def draw_console(screen):
+    """
+        fills with black the current console
+    """
     dimentions = pygame.Rect(750, 250, 400, 450)
     pygame.draw.rect(screen, [0, 0, 0], dimentions)
 
@@ -68,13 +104,13 @@ def show_button(screen, x, y, button_width, button_height,  button_color, text_f
     screen.blit(text, (x + 5, y + button_height/3))
 
 
-def handle_exit():
+def handle_exit(events):
     """
         key_input handler\n
         return True or False\n
     """
     Playing = True
-    for event in pygame.event.get():
+    for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 Playing = False
@@ -83,17 +119,97 @@ def handle_exit():
     return Playing
 
 
-def is_button_pressed(btn_x, btn_y, btn_width, btn_height):
+def draw_hand(screen, x, y, hand):
+    translate = 20
+    for letterIndex in range(len(hand)):
+        draw_text(screen, x + translate*letterIndex, y,
+                  hand[letterIndex], [255, 0, 0], 'Corbel', 30)
+
+
+def draw_bullet_points(screen, x, y, theList):
+    translate = 30
+    for itemIndex in range(len(theList)):
+        draw_text(screen, x, y + translate*itemIndex,
+                  theList[itemIndex], [255, 0, 0], 'Corbel', 30)
+
+
+def draw_buttons(screen, button_width, button_height, button_font):
+    pygame.draw.rect(screen, [50, 50, 50], [750, 0, 350, 250])
+    # ----------- BUTTON 2
+    button2_x = 780
+    button2_y = 20
+    text_color = [0, 0, 0]
+    show_button(screen, button2_x, button2_y, button_width,
+                button_height, [100, 100, 100], button_font, 'Skip', 18, text_color)
+
+    # ----------- BUTTON 3
+    button3_x = 780
+    button3_y = 90
+    text_color = [0, 0, 0]
+    show_button(screen, button3_x, button3_y, button_width,
+                button_height, [100, 100, 100], button_font, 'Exchange', 18, text_color)
+
+    # ----------- BUTTON 4
+    button4_x = 780
+    button4_y = 150
+    text_color = [0, 0, 0]
+    show_button(screen, button4_x, button4_y, button_width,
+                button_height, [100, 100, 100], button_font, 'Place horizontal', 18, text_color)
+
+    # ----------- BUTTON 5
+    button5_x = 780
+    button5_y = 200
+    text_color = [0, 0, 0]
+    show_button(screen, button5_x, button5_y, button_width,
+                button_height, [100, 100, 100], button_font, 'Place vertical', 18, text_color)
+
+    return [[button2_x, button2_y], [button3_x, button3_y], [button4_x, button4_y], [button5_x, button5_y]]
+
+
+def player_console(screen, playerName, playerHand, playerScore, best_words):
+    """
+        takes the current player Name, and shows the current Players console
+    """
+
+    # name
+    draw_text(screen, 760, 260, "Player Name: ", [255, 255, 255], "Corbel", 30)
+    draw_text(screen, 950, 260, playerName, [255, 0, 0], "Corbel", 30)
+    # hand
+    draw_text(screen, 760, 360, "Player Hand: ", [255, 255, 255], "Corbel", 30)
+    draw_hand(screen, 920, 360, playerHand)
+
+    draw_text(screen, 760, 460, "Best Option: ",
+              [255, 255, 255], "Corbel", 30)
+    draw_bullet_points(screen, 920, 460, best_words)
+
+
+def is_button_pressed(events, btn_x, btn_y, btn_width, btn_height):
     """
         if the user presses the button, return True
     """
-
     coords = pygame.mouse.get_pos()
-
     if coords[0] > btn_x and coords[0] < btn_x + btn_width \
             and coords[1] > btn_y and coords[1] < btn_y + btn_height:
-        for event in pygame.event.get():
+        for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     return True
     return False
+
+
+def get_cell_pressed(events):
+    """
+        returns the cellX_index and cellY_index, \n
+        based on the position of the mouse
+    """
+    coords = pygame.mouse.get_pos()
+    if coords[0] < 700:
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print('coords: ', coords[0], coords[1])
+                    cellX_index = (coords[0]+1) // 50
+                    cellY_index = (coords[1]+1) // 50
+                    print("returning: ", cellX_index, cellY_index)
+                    return cellX_index, cellY_index
+    return -1, -1
